a4ed53058bcb732664b8fe46c9ad3e664fc6b933
rogeliodc <irdcruz@gmail.com>
2022-08-22 16:42:43 -0500

    middle

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const puerto = 3000
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    let metodo = req.method
+    console.log('Request Type:', req.method);
+    if (metodo != 'GET' && metodo != 'DELETE') {
+        res.sendStatus(405)
+    } else {
+        next();
+    }
+});
+
 app.get('/', (req, res) => {
     res.send("API : " );
 });
